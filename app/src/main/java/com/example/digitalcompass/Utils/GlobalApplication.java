8f2c476b87782a3d7d4805236c5676bf51d79c6c
Hien Vu <hienvu0893@gmail.com>
2020-02-03 00:01:22 +0700

    uupdate
    dsa

---
+++ app/src/main/java/com/example/digitalcompass/Utils/GlobalApplication.java
@@ -0,0 +1,6 @@
+package com.example.digitalcompass.Utils;
+
+import android.app.Application;
+
+public class GlobalApplication extends Application {
+}
--- app/src/main/java/com/example/digitalcompass/fragment/FragmentLocation.java
+++ app/src/main/java/com/example/digitalcompass/fragment/FragmentLocation.java
@@ -1,110 +1,93 @@
 package com.example.digitalcompass.fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.digitalcompass.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentLocation#newInstance} factory method to
- * create an instance of this fragment.
- */
+import es.dmoral.toasty.Toasty;
+
+
 public class FragmentLocation extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ImageView imageViewcopyAddress, imageViewcopyLat, imageViewcopyLon;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public FragmentLocation() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentLocation.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentLocation newInstance(String param1, String param2) {
-        FragmentLocation fragment = new FragmentLocation();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        initPermission();
+        init(view);
+        listener();
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+
+
+    private void init(View view) {
+        imageViewcopyAddress=(ImageView) view.findViewById(R.id.imageviewCopyAddress_Location);
+        imageViewcopyAddress.setColorFilter(getActivity().getResources().getColor(R.color.mbrow));
+        imageViewcopyLat=(ImageView) view.findViewById(R.id.imageviewCopyLat_Location);
+        imageViewcopyLat.setColorFilter(getActivity().getResources().getColor(R.color.mbrow));
+        imageViewcopyLon=(ImageView) view.findViewById(R.id.imageviewCopyLon_Location);
+        imageViewcopyLon.setColorFilter(getActivity().getResources().getColor(R.color.mbrow));
+    }
+    private void listener() {
+
+        imageViewcopyAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.normal(getActivity(),"Has copied the Address to the clipboard").show();
+
+            }
+        });
+        imageViewcopyLat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.normal(getActivity(),"Has copied the Latitude to the clipboard").show();
+
+            }
+        });
+        imageViewcopyLon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.normal(getActivity(),"Has copied the Lontitude to the clipboard").show();
+
+            }
+        });
+    }
+
+    public void initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                //Permisson don't granted
+                if (shouldShowRequestPermissionRationale(
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Toast.makeText(getActivity(), "Permission isn't granted ", Toast.LENGTH_SHORT).show();
+                }
+                // Permisson don't granted and dont show dialog again.
+                else {
+                    Toast.makeText(getActivity(), "Permisson don't granted and dont show dialog again ", Toast.LENGTH_SHORT).show();
+                }
+                //Register permission
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
         }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
